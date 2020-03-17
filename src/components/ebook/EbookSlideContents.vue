4fd3714ec40807a4d096e2b3eaeb7395d7db18e5
Weblittlebird <linxiao9798@163.com>
2020-03-17 11:13:02 +0800

    m 目录

---
+++ src/components/ebook/EbookSlideContents.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="ebook-slide-contents">
+      asdfg
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss' scoped>
+@import '../../assets/styles/global';
+</style>
